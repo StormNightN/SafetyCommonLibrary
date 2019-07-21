f2717a6ca396acad096de2b6984e531017387bbe
stormnight <stormnight@gmail.com>
2019-07-21 17:25:40 +0200

    Add OpenSSLInitializing function

---
+++ CommonSSLFunctions.cpp
@@ -0,0 +1,29 @@
+#include "CommonSSLFunctions.h"
+
+#include <openssl/bio.h>
+#include <openssl/ssl.h>
+#include <openssl/err.h>
+#include <mutex>
+
+
+namespace OpenSSLCommon
+{
+    /**
+     * METHOD NAME OpenSSLCommon::OpenSSLInitializing
+     */
+    void OpenSSLInitializing()
+    {
+        static bool wasInitialized = false;
+        static std::mutex openSSLInitializationGuard;
+        std::lock_guard<std::mutex> guard{ openSSLInitializationGuard };
+
+        if(!wasInitialized)
+        {
+            SSL_load_error_strings();
+            ERR_load_BIO_strings();
+            OpenSSL_add_all_algorithms();
+
+            SSL_library_init();
+        }
+    }
+}
--- CommonSSLFunctions.h
+++ CommonSSLFunctions.h
@@ -5,4 +5,15 @@
 #ifndef SAFETYCOMMON_COMMONSSLFUNCTIONS_H
 #define SAFETYCOMMON_COMMONSSLFUNCTIONS_H
 
+/**
+ * Contain common functions for openssl.
+ */
+namespace OpenSSLCommon
+{
+    /**
+     * Function, which does OpenSSL initialization.
+     * On thread safety manner.
+     */
+    void OpenSSLInitializing();
+}
 #endif //SAFETYCOMMON_COMMONSSLFUNCTIONS_H
