ac155958514c2602bab63e2c0f5e9d6d585c41aa
stormnight <stormnight@gmail.com>
2019-07-21 17:33:53 +0200

    Add CreateOpenSSLContext

--- CommonSSLFunctions.h
+++ CommonSSLFunctions.h
@@ -8,6 +8,9 @@
 /**
  * Contain common functions for openssl.
  */
+#include <openssl/ossl_typ.h>
+#include <openssl/ssl.h>
+
 namespace OpenSSLCommon
 {
     /**
@@ -15,5 +18,17 @@ namespace OpenSSLCommon
      * On thread safety manner.
      */
     void OpenSSLInitializing();
+    
+    /**
+     * Create SSL context
+     *
+     * @return  SSL_CTX is not equal null, if initialization was
+     *          success, false otherwise
+     */
+    inline SSL_CTX* CreateOpenSSLContext(bool isServer)
+    {
+        return isServer ? SSL_CTX_new(SSLv23_server_method()) :
+            SSL_CTX_new(SSLv23_client_method());
+    }
 }
 #endif //SAFETYCOMMON_COMMONSSLFUNCTIONS_H
